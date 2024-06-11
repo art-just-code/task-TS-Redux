340492698125b13f32e567613bfd94cbc68795d9
Grebnik89 <116430298+art-just-code@users.noreply.github.com>
2024-06-11 12:35:38 +0300

    created next app & dev branch

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={Styles["main"]}>
+      <div>
+        <p>Home</p>
+      </div>
+    </main>
+  );
+}
